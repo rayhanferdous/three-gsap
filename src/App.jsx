ee52f2ba2b0c3ec8fd3db84bf2e95d61ccffff6e
rayhanferdous <mferdous152079@bba.uiu.ac.bd>
2024-12-02 12:14:40 +0600

    add tailwind and gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useGSAP(() => {
+    gsap.to(".size-14", {
+      duration: 1,
+      // delay: 2,
+      x: "50vw",
+      repeat: -1,
+      rotation: 360,
+      width: "100px",
+      height: "100px",
+      yoyo: true,
+      ease: "bounce.out",
+    });
+
+    gsap.to(".svgBox", {
+      duration: 2,
+      x: 100, // use transform shorthand (this is now using SVG units not px, the SVG viewBox is 100 units wide)
+      xPercent: -100,
+      // or target SVG attributes
+      attr: {
+        fill: "#8d3dae",
+        rx: 50,
+      },
+      repeat: -1,
+      yoyo: true,
+      ease: "back.out",
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="w-full">
+      <div className="size-14 rounded-md bg-red-300"></div>
+      <svg id="svg" viewBox="0 0 100 100">
+        <rect
+          className="svgBox"
+          fill="#28a92b"
+          x="0"
+          y="35"
+          width="30"
+          height="30"
+          rx="2"
+        />
+      </svg>
+    </div>
+  );
 }
 
-export default App
+export default App;
